64afd3e2364d2dfe453c25f64887268c0e6336f0
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-03-28 08:21:41 +0530

    encapsulation concept

---
+++ day_5_monday/encapsulation.py
@@ -0,0 +1,27 @@
+# Encapsulation is one of the key features of object-oriented programming. Encapsulation refers to the bundling of attributes and methods inside a single class.
+# It prevents outer classes from accessing and changing attributes and methods of a class. This also helps to achieve data hiding.
+
+# In Python, we denote private attributes using underscore as the prefix i.e single _ or double __
+
+
+class Computer:
+
+    def __init__(self):
+        self.__maxprice = 9000
+
+    def sell(self):
+        print(f"Selling Price: {self.__maxprice}")
+
+    def setMaxPrice(self, price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+# change the price
+c.__maxprice = 1000
+c.sell()
+
+# using setter function
+c.setMaxPrice(1000)
+c.sell()
