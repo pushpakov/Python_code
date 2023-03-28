6b98f2fa1bf48bc3821d48fa9754dc3bce278b26
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-03-28 19:46:58 +0530

    multiprocessing added with example

---
+++ Day_6_tuesday/multiprocessing_queue_class.py
@@ -0,0 +1,23 @@
+from multiprocessing import Queue
+
+colors = ['red', 'green', 'blue', 'black']
+cnt = 1
+# instantiating a queue object
+queue = Queue()
+print('pushing items to queue:')
+for color in colors:
+    print('item no: ', cnt, ' ', color)
+    queue.put(color)
+    cnt += 1
+
+print('\npopping items from queue:')
+cnt = 1
+while not queue.empty():
+    print('item no: ', cnt, ' ', queue.get())
+    cnt += 1
+
+
+
+
+
+    
