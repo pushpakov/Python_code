1a85e2c07b71a1f2639d6aa22837700ee4b8b657
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-03-28 08:50:38 +0530

    polymorphism concept

--- day_5_monday/polymorphism.py
+++ day_5_monday/polymorphism.py
@@ -3,22 +3,20 @@
 
 
 class Polygon:
-    # method to render a shape
-    def render(self):
+    # render a shape
+    def render(self) -> None:
         print("Rendering Polygon...")
 
 class Square(Polygon):
     # renders Square
-    def render(self):
+    def render(self) -> None:
         print("Rendering Square...")
 
 class Circle(Polygon):
     # renders circle
-    def render(self):
+    def render(self) -> None:
         print("Rendering Circle...")
     
-# here same method is called by different object and is giving different output  
-
 # create an object of Square
 s1 = Square()
 s1.render()
@@ -27,6 +25,39 @@ s1.render()
 c1 = Circle()
 c1.render()
 
-# create an object of Polygon
-p1 = Polygon()
-p1.render() 
+try:
+    # create an object of Polygon (which is abstract)
+    p = Polygon()
+    p.render()
+except TypeError as e:
+    print(f"Error: {e}")
+
+
+
+
+
+
+
+# another example 
+print("OTHER EXAMPLE  : ----------------------------------------------")
+
+class Animal:                 #  abstract class
+    def __init__(self, name):
+        self.name = name
+    
+    def speak(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow meow!"
+
+dog = Dog("Rufus")
+cat = Cat("Whiskers")
+
+print(dog.speak())  #  "Woof woof!"
+print(cat.speak())  #  "Meow meow!"
