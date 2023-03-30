87e13d8037a20bb3ecf077b3ceee647e83beca1f
PuShPaK-kUmAr <pushpak.prabhakar@anywhere.co>
2023-03-30 18:48:30 +0530

    write table using flask

---
+++ Day_8_Thursday/app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+class InvalidInputError(Exception):
+    pass
+
+app = Flask(__name__)
+
+@app.route('/table/<int:num>')
+def table(num: int) -> str:
+    try:
+        if num < 1:
+            raise InvalidInputError("Number must be positive")
+        return render_template('table.html', n=num)
+    except InvalidInputError as e:
+        return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
